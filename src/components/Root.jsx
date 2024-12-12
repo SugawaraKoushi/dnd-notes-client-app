3ab930c88caf18f88b871e07736c999a9215177a
Vladislav <senior.vlados97@gmail.com>
2024-12-12 20:08:02 +0300

    side menu investigation

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,13 +1,21 @@
-import { Layout } from "antd";
+import { Layout, Menu } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
+import { useState } from "react";
 
 const Root = () => {
+    const [theme, setTheme] = useState("dark");
+    const items = [{ key: "0", label: "Кампании" }];
+
     return (
-        <Layout>
+        <Layout style={{ height: "100vh" }}>
             <Header></Header>
-            <Layout>
-                <Sider></Sider>
+            <Layout
+                style={{ margin: "auto", width: "100%", maxWidth: "1270px" }}
+            >
+                <Sider>
+                    <Menu theme={theme} items={items} />
+                </Sider>
                 <Content></Content>
             </Layout>
             <Footer></Footer>
