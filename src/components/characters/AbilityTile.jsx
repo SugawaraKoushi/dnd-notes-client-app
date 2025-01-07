e6d65da8cc8e1a2259c56979b06c4b72942f6b8c
Vladislav Fokin <senior.vlados97@gmail.com>
2025-01-07 21:02:32 +0300

    Implement ability value change

--- src/components/characters/AbilityTile.jsx
+++ src/components/characters/AbilityTile.jsx
@@ -1,9 +1,41 @@
 import { DownOutlined, UpOutlined } from "@ant-design/icons";
 import { Button, Flex } from "antd";
+import { useEffect, useState } from "react";
 
 const AbilityTile = (props) => {
+    const [modifier, setModifier] = useState("+0");
+
+    useEffect(() => {
+        calculateModifier();
+    });
+
     const onIncrementButtonClick = () => {
-        props.increment();
+        if (props.value === 20) {
+            return;
+        }
+
+        props.setValue(props.value + 1);
+        calculateModifier();
+    };
+
+    const onDecrementButtonClick = () => {
+        if (props.value === 0) {
+            return;
+        }
+
+        props.setValue(props.value - 1);
+        calculateModifier();
+    };
+
+    const calculateModifier = () => {
+        const modifierValue = Math.floor((props.value - 10) / 2);
+
+        if (modifierValue >= 0) {
+            setModifier(`+${modifierValue}`);
+            return;
+        }
+
+        setModifier(modifierValue);
     };
 
     return (
@@ -13,6 +45,7 @@ const AbilityTile = (props) => {
                 type="outlined"
                 icon={<UpOutlined />}
                 block
+                disabled={props.value === 20}
             />
             <Flex
                 vertical
@@ -39,9 +72,15 @@ const AbilityTile = (props) => {
                 >
                     {props.value}
                 </span>
-                <span>+0</span>
+                <span>{modifier}</span>
             </Flex>
-            <Button type="outlined" icon={<DownOutlined />} block />
+            <Button
+                onClick={onDecrementButtonClick}
+                type="outlined"
+                icon={<DownOutlined />}
+                block
+                disabled={props.value === 0}
+            />
         </div>
     );
 };
--- src/components/characters/NewCharacter.jsx
+++ src/components/characters/NewCharacter.jsx
@@ -1,23 +1,24 @@
 import { Flex, List } from "antd";
-import AbilityTile from "../abilities/AbilityTile";
+import AbilityTile from "./AbilityTile";
 import { useState } from "react";
 
 const NewCharacter = () => {
     const [strength, setStrength] = useState(10);
-    const incrementStrength = (value) => {
-        value++;
-        setStrength(value);
-    };
+    const [dexterity, setDexterity] = useState(10);
+    const [constitution, setConstitution] = useState(10);
+    const [intelligence, setIntelligence] = useState(10);
+    const [wisdom, setWisdom] = useState(10);
+    const [charisma, setCharisma] = useState(10);
 
     return (
         <>
             <Flex wrap gap="small" justify="space-between">
-                <AbilityTile name="сила" value={strength} increment={incrementStrength} />
-                <AbilityTile name="ловкость" value={10} />
-                <AbilityTile name="телосложение" value={10} />
-                <AbilityTile name="интеллект" value={10} />
-                <AbilityTile name="мудрость" value={10} />
-                <AbilityTile name="харизма" value={10} />
+                <AbilityTile name="сила" value={strength} setValue={setStrength} />
+                <AbilityTile name="ловкость" value={dexterity} setValue={setDexterity} />
+                <AbilityTile name="телосложение" value={constitution} setValue={setConstitution} />
+                <AbilityTile name="интеллект" value={intelligence} setValue={setIntelligence} />
+                <AbilityTile name="мудрость" value={wisdom} setValue={setWisdom} />
+                <AbilityTile name="харизма" value={charisma} setValue={setCharisma} />
             </Flex>
         </>
     );
