37544d91aecda2105f7db9a0531459e22e629fce
Vladislav Fokin <senior.vlados97@gmail.com>
2025-01-08 21:37:19 +0300

    Add checkbox

--- src/components/characters/AbilityCheck.jsx
+++ src/components/characters/AbilityCheck.jsx
@@ -1,11 +1,14 @@
-import { Button, Flex } from "antd";
+import { Button, Checkbox, Flex } from "antd";
 import { Link } from "react-router";
 import "./index.css";
 
 const AbilityCheck = (props) => {
     return (
         <Flex className="ability-check" justify="space-beetwen" align="center">
-            <Flex className="ability-check-wrap">
+            <Flex align="center" className="ability-check-wrap">
+                {props.checkable && (
+                    <Checkbox className="ability-check-checkbox" />
+                )}
                 <Link className="ability-check-label" to="">
                     {props.name.toUpperCase()}
                 </Link>
