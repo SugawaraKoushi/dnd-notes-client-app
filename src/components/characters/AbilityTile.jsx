83468fc8a30b298ec602de4d362b06f7ffc9f5fd
Vladislav Fokin <senior.vlados97@gmail.com>
2025-03-28 21:39:56 +0300

    Add unique key prop

--- src/components/characters/AbilityTile.jsx
+++ src/components/characters/AbilityTile.jsx
@@ -1,25 +1,50 @@
 import { Flex, Typography } from "antd";
 import "./index.css";
 import AbilityCheck from "./AbilityCheck";
-import { calculateModifier, calculateModifierAsString } from "../services/ModifierService";
+import {
+    calculateModifier,
+    calculateModifierAsString,
+} from "../services/ModifierService";
 
 const AbilityTile = (props) => {
     const { Title } = Typography;
-    const abilityModalTitle = `${props.name} ${calculateModifierAsString(props.value)}`;
+    const abilityModalTitle = `${props.name} ${calculateModifierAsString(
+        props.value
+    )}`;
 
     return (
         <Flex className="ability-tile" id={props.id} vertical>
             <Flex className="ability-tile-header" justify="space-between">
-                <Title level={3} >{props.name.toUpperCase()}</Title>
+                <Title level={3}>{props.name.toUpperCase()}</Title>
                 <Title level={3}>{props.value}</Title>
             </Flex>
             <Flex className="ability-checks">
-                <AbilityCheck name="проверка" skill={false} modalTitle={abilityModalTitle} value={calculateModifier(props.value)}/>
-                <AbilityCheck name="спасбросок" skill={false} modalTitle={abilityModalTitle} value={calculateModifier(props.value)} checkable={true} />
+                <AbilityCheck
+                    key={props.name + props.value + 1}
+                    name="проверка"
+                    skill={false}
+                    modalTitle={abilityModalTitle}
+                    value={calculateModifier(props.value)}
+                />
+                <AbilityCheck
+                    key={props.name + props.value + 2}
+                    name="спасбросок"
+                    skill={false}
+                    modalTitle={abilityModalTitle}
+                    value={calculateModifier(props.value)}
+                    checkable={true}
+                />
             </Flex>
-            {props.skills && props.skills.map((skill, i) => (
-                <AbilityCheck name={skill} skill={true} value={calculateModifier(props.value)} checkable={true} />
-            ))}
+            {props.skills &&
+                props.skills.map((skill, i) => (
+                    <AbilityCheck
+                        key={props.name + props.value + i}
+                        name={skill}
+                        skill={true}
+                        value={calculateModifier(props.value)}
+                        checkable={true}
+                    />
+                ))}
         </Flex>
     );
 };
